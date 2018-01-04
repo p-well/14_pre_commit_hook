1477d885b52e785932a3545dfd7d65afb0417330
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-01-04 12:21:08 +0300

    3rd test commit

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -6,7 +6,7 @@ def get_roots(a, b, c):
     if discriminant < 0:
         return None, None
     root1 = (-b - sqrt(discriminant)) / (2 * a)
-    root2 = (b + sqrt(discriminant)) / (2 * a)
+    root2 = (-b + sqrt(discriminant)) / (2 * a)
     if discriminant == 0:
         return root1, None
     else:
